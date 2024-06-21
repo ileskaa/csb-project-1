e1e037a26b11a0a3b6f54b40e385da090630d333
ileskaa <akseli.ingervo@gmail.com>
2024-06-21 12:07:13 +0300

    Complete part 5

--- brokenapp/views.py
+++ brokenapp/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 from django.views import generic
+from django.utils import timezone
 
 from .models import Choice, Question
 
@@ -24,8 +25,9 @@ class IndexView(generic.ListView):
     context_object_name = "latest_question_list"
 
     def get_queryset(self):
-        """Return the last five published questions."""
-        return Question.objects.order_by("-pub_date")[:5]
+        """Return the last five published questions (excluding those set to be
+    published in the future)."""
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by("-pub_date")[:5]
 
 
 def get_question(question_id):
@@ -42,6 +44,12 @@ def get_question(question_id):
 class DetailView(generic.DetailView):
     model = Question
     template_name = "brokenapp/detail.html"
+
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet.
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 
 class ResultsView(generic.DetailView):
