1e3f2e5a35ca446651a0f0a8c2166882d1fb7b6d
ileskaa <akseli.ingervo@gmail.com>
2024-07-10 22:09:24 +0300

    Fix request path

--- brokenapp/views.py
+++ brokenapp/views.py
@@ -66,7 +66,7 @@ def vote(request, question_id):
         # Redisplay the question voting form.
         return render(
             request,
-            "polls/detail.html",
+            "brokenapp/detail.html",
             {
                 "question": question,
                 "error_message": "You didn't select a choice.",
