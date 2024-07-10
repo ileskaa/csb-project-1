986e78125511b03b29889c51cd916d914b71c12f
ileskaa <akseli.ingervo@gmail.com>
2024-07-10 22:08:04 +0300

    Add explanative comment

--- brokenapp/urls.py
+++ brokenapp/urls.py
@@ -5,6 +5,7 @@ from . import views
 # Add a namespace
 app_name = "brokenapp"
 urlpatterns = [
+    # as_view() is required for class-based views
     path("", views.IndexView.as_view(), name="index"),
     path("<int:pk>/", views.DetailView.as_view(), name="detail"),
     path("<int:pk>/results/", views.ResultsView.as_view(), name="results"),
