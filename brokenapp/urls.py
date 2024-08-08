9ab456ad18bd7972ebeae88b950408adf00e2fea
ileskaa <akseli.ingervo@gmail.com>
2024-08-08 17:56:47 +0300

    Add comment

--- brokenapp/urls.py
+++ brokenapp/urls.py
@@ -7,6 +7,8 @@ app_name = "brokenapp"
 urlpatterns = [
     # as_view() is required for class-based views
     path("", views.IndexView.as_view(), name="index"),
+    # angle brackets are used to capture part of the URL
+    # and send it as a keyword arg to the view
     path("<int:pk>/", views.DetailView.as_view(), name="detail"),
     path("<int:pk>/results/", views.ResultsView.as_view(), name="results"),
     path("<int:question_id>/vote/", views.vote, name="vote"),
